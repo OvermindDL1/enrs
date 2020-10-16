159159e9304659d857f71da20dcb549b4d3c7723
OvermindDL1 <overminddl1@gmail.com>
2020-10-16 14:06:06 -0600

    Made features work to control whether the other ECS's are benchmarked or not

--- benches/other_ecs/mod.rs
+++ benches/other_ecs/mod.rs
@@ -1,2 +1,14 @@
+#[cfg(feature = "legion")]
 pub mod legion;
+#[cfg(feature = "shipyard")]
 pub mod shipyard;
+
+#[cfg(not(feature = "legion"))]
+pub mod legion {
+	pub fn benchmarks() {}
+}
+
+#[cfg(not(feature = "shipyard"))]
+pub mod shipyard {
+	pub fn benchmarks() {}
+}
